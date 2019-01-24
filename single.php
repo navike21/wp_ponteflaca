3f8ac9fdd8584593acbe16003a63e320deca5a23
navike21 <navike21@gmail.com>
2019-01-24 14:08:54 -0500

    asdfasdfas

--- single.php
+++ single.php
@@ -3,12 +3,12 @@
   get_header(); 
 
   if ( have_posts() ) : the_post(); 
-    echo '<section class="section_middle_center w_100 ">';
+    echo '<section class="habitos section_middle_center w_100 ">';
       the_title('<h2>','</h2>');
       echo '<section class="section_top_center portadas_blog parallax-container" data-speed=".5" data-parallax="scroll" data-position="center" data-image-src="'; the_post_thumbnail_url();echo '">'; echo '</section>';
       echo '<article class="section_top_justify">';
         the_content();
-        echo '<section class="w_100">';
+        echo '<section class="section_bottom_center w_100">';
         comments_template();
         // echo '<a href="'; the_permalink(); echo'">Enviar >>'; echo '</a>';
         echo '<section>';
