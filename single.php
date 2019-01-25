35af2d7b19ef8b2c0ee2f8c93d39584762ba84b6
navike21 <navike21@gmail.com>
2019-01-25 09:20:51 -0500

    ADSAS
    Signed-off-by: navike21 <navike21@gmail.com>

--- single.php
+++ single.php
@@ -2,8 +2,8 @@
 <?php 
   get_header(); 
 
+  echo '<section class="habitos section_middle_center w_100 ">';
   if ( have_posts() ) : the_post(); 
-    echo '<section class="habitos section_middle_center w_100 ">';
       the_title('<h2>','</h2>');
       echo '<section class="section_top_center portadas_blog parallax-container" data-speed=".5" data-parallax="scroll" data-position="center" data-image-src="'; the_post_thumbnail_url();echo '">'; echo '</section>';
       echo '<article class="section_top_justify">';
@@ -13,8 +13,8 @@
           comments_template();
         echo '<div>';
       echo '</aticle>';
-    echo '</section>';
-  endif;    
+    endif;    
+  echo '</section>';
 
   get_footer(); 
 ?>
